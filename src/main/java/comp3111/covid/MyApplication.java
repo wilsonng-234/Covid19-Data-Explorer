d9e2da188fb6bda72ccd50507ceb973a3f9bede4
lhyuenad <lhyuenad@CCVDIPS6105.ust.hk>
2022-04-07 00:07:42 +0900

    test

--- src/main/java/comp3111/covid/MyApplication.java
+++ src/main/java/comp3111/covid/MyApplication.java
@@ -47,7 +47,7 @@ public class MyApplication extends Application {
    		VBox root = (VBox) loader.load();
    		Scene scene =  new Scene(root);
    		stage.setScene(scene);
-   		stage.setTitle("Lovelive Superstar Liella : Data Explorer on COVID-19");
+   		stage.setTitle("Lovelive Superstar Liella : Data Explorer on COVID-19 Test");
 
    		stage.show();
 	}
