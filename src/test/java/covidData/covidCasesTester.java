53820c03e3f3201aa9a843cb1301a32bc3e26c05
wilsonng <wilsonng234@gmail.com>
2022-04-17 23:46:36 +0800

    Test covidCases Data

---
+++ src/test/java/covidData/covidCasesTester.java
@@ -0,0 +1,90 @@
+package covidData;
+
+import org.junit.Test;
+
+import static comp3111.covid.DataAnalysis.getConfirmedCases;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class covidCasesTester {
+	Date date;
+	int[] targetTotalCases;
+	double[] targetCasesPerMillion;
+	String dataset;
+
+	@Before
+	public void setup(){
+		date = new Date(2022,4,7);
+		targetTotalCases = new int[]{11_965,31_216_337,854_434};
+		targetCasesPerMillion = new double[]{1595.974,22620.462,6706.56};
+		dataset = "COVID_Dataset_v1.0.csv";
+	}
+
+	@Test
+	public void confirmedNewCases(){
+		String str = getConfirmedCases(dataset,"HKG");
+		String target =
+				"Dataset (COVID_Dataset_v1.0.csv): 103,907 Records\n" +
+						"\n" +
+						"[Summary (HKG)]\n" +
+						"Number of Confirmed Cases: 11,965\n" +
+						"Number of Days Reported: 545\n";
+
+		assertTrue(str.equals(target));
+	}
+
+	@Test
+	public void confirmedTotalCasesWithValidDateAndCountries(){
+		Date date = new Date(2021, 7, 21);
+		String[] countries = new String[]{"HKG","IND","ISR"};
+
+		confirmedCases confirmedCases = new confirmedCases(date,countries,dataset);
+
+		assertArrayEquals(confirmedCases.getTotalCases(),targetTotalCases);
+	}
+
+	@Test
+	public void confirmedTotalCasesPerMillionWithValidDateAndCountries(){
+		Date date = new Date(2021, 7, 21);
+		String[] countries = new String[]{"HKG","IND","JPN"};
+
+
+		confirmedCases confirmedCases = new confirmedCases(date,countries,dataset);
+
+		System.out.println(confirmedCases.getCasesPerMillion()[0]);
+		assertArrayEquals(confirmedCases.getCasesPerMillion(),targetCasesPerMillion,0.001);
+	}
+
+	@Test
+	public void isDateBefore(){
+		assertTrue(new Date(2021,4,7).isBefore(date));
+		assertTrue(new Date(2022,3,7).isBefore(date));
+		assertTrue(new Date(2022,4,6).isBefore(date));
+
+		assertFalse(new Date(2022,4,7).isBefore(date));
+
+		assertFalse(new Date(2022,4,8).isBefore(date));
+		assertFalse(new Date(2022,5,7).isBefore(date));
+		assertFalse(new Date(2023,4,7).isBefore(date));
+	}
+
+	@Test
+	public void isDateEqual(){
+		assertTrue(new Date(2022,4,7).isEqual(date));
+	}
+
+	@Test
+	public void isDateAfter(){
+		assertFalse(new Date(2021,4,7).isAfter(date));
+		assertFalse(new Date(2022,3,7).isAfter(date));
+		assertFalse(new Date(2022,4,6).isAfter(date));
+
+		assertFalse(new Date(2022,4,7).isAfter(date));
+
+		assertTrue(new Date(2022,4,8).isAfter(date));
+		assertTrue(new Date(2022,5,7).isAfter(date));
+		assertTrue(new Date(2023,4,7).isAfter(date));
+	}
+}
+
+
