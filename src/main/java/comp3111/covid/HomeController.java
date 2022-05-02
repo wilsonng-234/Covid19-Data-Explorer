164915ee33b74250d0aebac816225cb9f297c2f5
wilsonng <wilsonng234@gmail.com>
2022-05-03 00:19:13 +0800

    Create javadoc

--- src/main/java/comp3111/covid/HomeController.java
+++ src/main/java/comp3111/covid/HomeController.java
@@ -15,6 +15,11 @@ public class HomeController {
     @FXML
     private ImageView ConfirmedCasesIcon;
 
+    /**
+     * switch to confirmed cases scene
+     * @param event ConfirmedCasesIcon is clicked
+     * @throws IOException
+     */
     @FXML
     void switchToConfirmedCasesScene(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/ui/confirmedCases.fxml"));
@@ -27,14 +32,24 @@ public class HomeController {
     @FXML
     private ImageView DeathCasesIcon;
 
+    /**
+     * switch to death cases scene
+     * @param event DeathCasesIcon is clicked
+     * @throws IOException
+     */
     @FXML
-    void switchToDeathCasesScene(MouseEvent event) {
+    void switchToDeathCasesScene(MouseEvent event) throws IOException {
 
     }
 
     @FXML
     private ImageView VaccinationIcon;
 
+    /**
+     * switch to rate of vaccination scene
+     * @param event VaccinationIcon is clicked
+     * @throws IOException
+     */
     @FXML
     void switchToRateOfVaccinationScene(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/ui/vaccinationRate.fxml"));
@@ -47,6 +62,11 @@ public class HomeController {
     @FXML
     private ImageView LinearRegressionIcon;
 
+    /**
+     * switch to linear regression scene
+     * @param event linear regression icon is clicked
+     * @throws IOException
+     */
     @FXML
     void switchToLinearRegressionScene(MouseEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/ui/LinearRegression.fxml"));
