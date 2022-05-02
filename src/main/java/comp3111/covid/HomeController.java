baedaa3adcb2cd53f1fb12c0183a26a1bbc45b51
wilsonng <wilsonng234@gmail.com>
2022-05-03 01:06:40 +0800

    minor fix on javadoc

--- src/main/java/comp3111/covid/HomeController.java
+++ src/main/java/comp3111/covid/HomeController.java
@@ -11,6 +11,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * HomeController initializes home scene
+ */
 public class HomeController {
     @FXML
     private ImageView ConfirmedCasesIcon;
